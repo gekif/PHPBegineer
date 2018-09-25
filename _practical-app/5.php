60614312f781acb863aee8bedadf19739150a58c
Gekif <dzulfikar.maulana@gmail.com>
2018-09-25 20:30:30 +0700

    Practical-App5: Built in Function

--- _practical-app/5.php
+++ _practical-app/5.php
@@ -17,13 +17,22 @@
 
 /*  Step1: Use a pre-built math function here and echo it
 
-
 	Step 2:  Use a pre-built string function here and echo it
 
-
 	Step 3:  Use a pre-built Array function here and echo it
-
  */
+
+// Step 1
+echo sqrt(100);
+echo '<br>';
+
+// Step 2
+echo strlen('Kuaicong tai');
+echo '<br>';
+
+// Step 3
+$list = [1,20,30,40];
+echo max($list);
 
 	
 ?>
