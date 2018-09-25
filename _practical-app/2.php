f0adb2d3839b73a0423bda2b5d199f686e75b1ef
Gekif <dzulfikar.maulana@gmail.com>
2018-09-25 20:01:04 +0700

    Practical-App2: Variables and Array

--- _practical-app/2.php
+++ _practical-app/2.php
@@ -21,17 +21,36 @@
 
 		  Step 2: Add the two variables and display the sum with echo:
 
-
 		  Step3: Make 2 Arrays with the same values, one regular and the other associative
 
 		  Step4: Make a constant and set it to the value of PHP. and use an echo to print it out
 
-
-			
-
 			 */
 
-		
+		// Step 1
+        $number1 = 2;
+        $number2 = 20;
+
+        // Step 2
+        echo "the sum of $number1 and $number2 is ";
+        echo $number1 + $number2;
+        echo '<br>';
+
+        // Step 3
+        $listOfNumbers = [ 20, 30, 40, 50, 60];
+        print_r($listOfNumbers);
+        echo '<br>';
+        echo "The sum of array is " . array_sum($listOfNumbers);
+        echo '<br>';
+
+        // Step 4
+        $assocArray = [
+                'first_name' => 'Dzulfikar',
+                'last_name' => 'Maulana'
+        ];
+
+        echo "My names is $assocArray[first_name] $assocArray[last_name] and i am $listOfNumbers[1] years old";
+
 
 
 		?>
