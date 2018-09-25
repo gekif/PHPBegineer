be9b7668f1de5dfc17b91ef2b71aa897b6dcdf41
Gekif <dzulfikar.maulana@gmail.com>
2018-09-25 21:01:24 +0700

    Practical-App6: Form Submit

--- _practical-app/6.php
+++ _practical-app/6.php
@@ -14,15 +14,21 @@
 <article class="main-content col-xs-8">
  
 
-	<?php  
+<?php
 
 /*  Step1: Make a form that submits one value to POST super global
-
-
  */
 
-	
+if(isset($_POST['submit'])) {
+    echo "Your Username is $_POST[username] and your password is $_POST[password]";
+}
+
 ?>
+<form action="6.php" method="post">
+    <input type="text" placeholder="username" name="username">
+    <input type="password" placeholder="password" name="password">
+    <input type="submit" name="submit" value="SUBMIT">
+</form>
 
 
 </article><!--MAIN CONTENT-->
