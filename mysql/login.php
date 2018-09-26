200b9acc3abb7d4f1fd1cae76df16f56186b25d4
Gekif <dzulfikar.maulana@gmail.com>
2018-09-27 05:29:50 +0700

    MySQLV0: Create Markup

---
+++ mysql/login.php
@@ -0,0 +1,31 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css">
+</head>
+<body>
+
+<div class="container">
+
+    <div class="col-xs-6">
+        <form method="post" action="login.php">
+            <div class="form-group">
+                <label for="username">Username</label>
+                 <input type="text" name="username" class="form-control">
+            </div>
+            <div class="form-group">
+                <label for="password">Password</label>
+                <input type="password" name="password" class="form-control">
+            </div>
+            <input class="btn btn-primary" type="submit" name="submit" value="Submit">
+    </div>
+
+</div>
+
+</body>
+</html>
