4499f1160d6d402c5f418ea0983f1977fec711b2
Gekif <dzulfikar.maulana@gmail.com>
2018-09-29 07:33:28 +0700

    Practical-App7: Databases

--- _practical-app/7.php
+++ _practical-app/7.php
@@ -1,4 +1,13 @@
 <?php include "functions.php" ?>
+
+<?php
+$connection = mysqli_connect('localhost', 'root', 'password', 'mylist');
+
+$query = "SELECT * FROM reports";
+$result = mysqli_query($connection, $query);
+
+?>
+
 <?php include "includes/header.php" ?>
     
 
@@ -18,17 +27,30 @@
 	
 	<?php  
 
-	/*  Step 1 - Create a database in PHPmyadmin
+	/*  Step 1 - Create a database in PHPmyadmin - mylist
 
-		Step 2 - Create a table like the one from the lecture
+		Step 2 - Create a table like the one from the lecture - report
 
-		Step 3 - Insert some Data
+		Step 3 - Insert some Data - done
 
 		Step 4 - Connect to Database and read data
 
 */
+
+    if (!$connection) {
+        die('Database died ' . mysqli_error($connection));
+    }
+
+    if (!$result) {
+        die('Query died ' . mysqli_error($connection));
+    }
+
+    while($row = mysqli_fetch_assoc($result)) {
+        echo "$row[days_of_the_week] on $row[months] <br>";
+    }
 	
 	?>
+
 
 
 
